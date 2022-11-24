0f962a51e8ed6668e4d88f49e340c05a30170f9a
GURAMI KIKNADZE <gurikiknadze@gmail.com>
2022-11-24 21:24:58 +0400

    maybe i'm the problem, not the code

--- index.js
+++ index.js
@@ -2,8 +2,24 @@
 // service_qb0dndl
 // yBtIX7eeMg3G6dYem
 
+let isModalOpen = false;
+let contrastToggle = false;
+
+function toggleContrast() {
+    contrastToggle = !contrastToggle;
+    if (contrastToggle) {
+        document.body.classList += " dark-theme"
+    }
+else { document.body.classList += " dark-theme"
+}
+}
+
+
 function contact(event) {
     event.preventDefault();
+    const loading = document.querySelector('.modal__overlay--loading')
+const success = document.querySelector('.modal__overlay--success')
+loading.classList.remove("modal__overlay--visible");
     emailjs
         .sendForm(
             'service_qb0dndl',
@@ -11,10 +27,33 @@ function contact(event) {
             event.target,
             'yBtIX7eeMg3G6dYem'
         ).then(() => {
-            console.log("this worked1")
+            loading.classList.remove("modal__overlay--visible");
+    success.classList += " modal__overlay--visible"
+        }).catch(() => {
+            
+            alert(
+                "The email service is temporarily unavialable. Please contact me directly on gurikiknadze@gmail.com"
+            )
         })
 
+
+
+
+loading.classList += " modal__overlay--visible" 
+setTimeout(() => {
+    
+    console.log('it worked 1')
+}, 1000)
 }
 
 
-//stopped at 02:01:00
+function toggleModal() {
+    
+    if (isModalOpen) {
+        isModalOpen = false;
+        return document.body.classList.remove("modal--open")
+    }
+        
+    isModalOpen = true;
+    document.body.classList += " modal--open"
+}
